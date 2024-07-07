bdd6d49203cebebd880e058657a8906fdd379873
yeon2lee <md04254@naver.com>
2024-07-07 20:13:28 +0900

    test: 디딤판의 가로 라인이 겹치지 않게 한다

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -10,4 +10,11 @@ public class LineTest {
         int n = 5;
         Assertions.assertEquals(4, new Line(n).getLayerSize());
     }
+
+    @Test
+    void 가로_라인이_겹치지_않게_한다() {
+        boolean value = true;
+        LineGenerator generator = new LineGenerator();
+        Assertions.assertEquals(false, generator.generate(value));
+    }
 }
