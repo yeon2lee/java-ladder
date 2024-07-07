77ba8e46119fbfc1d4e0291d2d8f0fe913ff0796
yeon2lee <md04254@naver.com>
2024-07-07 19:29:04 +0900

    feat: 참여자 수는 2명 이상이어야 한다

---
+++ src/main/java/domain/People.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.util.List;
+
+public class People {
+
+    private final List<Person> people;
+
+    public People(List<String> names) {
+        validate(names);
+        List<Person> people = names.stream().map(Person::new).toList();
+        this.people = people;
+    }
+
+    private void validate(List<String> names) {
+        if (names.size() < 2) {
+            throw new IllegalArgumentException("참여할 사람은 두 명 이상이어야 합니다.");
+        }
+    }
+}
