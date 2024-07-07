965f7b9ca3e02bcd7db64db7f5635c03ca3ffd08
yeon2lee <md04254@naver.com>
2024-07-07 20:04:11 +0900

    feat: 참여자 수에 따라 디딤판을 생성한다

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+
+    private List<Boolean> points;
+
+    public Line(int n) {
+        List<Boolean> points = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < n - 1; i++) {
+            points.add(random.nextBoolean());
+        }
+        this.points = points;
+    }
+
+    public int getLayerSize() {
+        return points.size();
+    }
+}
